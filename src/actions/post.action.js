9901e9b4cc5ec9292059d5227d4b7e6cf3c588ae
henriB <henri.bretseau@gmail.com>
2021-03-09 12:53:55 +0100

    CRUD Edit_post

--- src/actions/post.action.js
+++ src/actions/post.action.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export const GET_POSTS = "GET_POSTS";
 export const ADD_POSTS = "ADD_POSTS";
+export const EDIT_POST = "EDIT_POST";
 
 export const getPosts = () => {
   return (dispatch) => {
@@ -19,6 +20,19 @@ export const addPosts = (data) => {
       .post("http://localhost:3000/posts", data)
       .then((res) => {
         dispatch({ type: ADD_POSTS, payload: data });
+      })
+      .catch((err) => console.log(err));
+  };
+};
+export const editPost = (data) => {
+  return (dispatch) => {
+    return axios({
+      method: "put",
+      url: `http://localhost:3000/posts/${data.id}`,
+      data: { ...data },
+    })
+      .then((res) => {
+        dispatch({ type: EDIT_POST, payload: { ...data } });
       })
       .catch((err) => console.log(err));
   };
