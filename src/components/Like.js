4d2d8fdb30322d8c8d4342367a61c85d2b1924a2
henriB <henri.bretseau@gmail.com>
2021-03-09 15:29:26 +0100

    ADD LIKE

--- src/components/Like.js
+++ src/components/Like.js
@@ -1,9 +1,29 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addLike } from "../actions/post.action";
 import { addUserLike } from "../actions/user.action";
 
 const Like = ({ post }) => {
+  const user = useSelector((state) => state.userReducer);
+  const dispacth = useDispatch();
+  const handleLike = () => {
+    const postData = {
+      title: post.title,
+      author: post.author,
+      content: post.content,
+      likes: ++post.likes,
+      id: post.id,
+    };
+    const userData = {
+      pseudo: user[0].pseudo,
+      likes: ++user[0].likes,
+      id: user[0].id,
+    };
+    dispacth(addLike(postData));
+    dispacth(addUserLike(userData));
+  };
   return (
-    <div>
+    <div onClick={handleLike}>
       <img src="./icons/clap.png" className="clap" alt="clap" />
       <span>{post.likes}</span>
     </div>
